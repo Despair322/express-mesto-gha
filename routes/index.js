294263a0613db816a7dd062171f9a366a65ebae2
Evgeniy Dymov <krionic1@gmail.com>
2023-11-08 00:58:15 +0300

    Добавляет дефолтный роут

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const cardRouter = require('./cards');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardRouter);
+router.use('*', (req, res) => res.status(404).send({ message: 'No such route' }));
 
 module.exports = router;
